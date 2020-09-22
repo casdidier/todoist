5b205f67a6a5d70fdd8bbea24543dc4ccacaa6ee
casdidier <cheung.didier@gmail.com>
2020-09-22 16:21:21 +0200

    setting up firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyC2kpDaLr7bFIxWvR54V3oxmipbkmOplj4",
+  authDomain: "todoist-clone-react-ce198.firebaseapp.com",
+  databaseURL: "https://todoist-clone-react-ce198.firebaseio.com",
+  projectId: "todoist-clone-react-ce198",
+  storageBucket: "todoist-clone-react-ce198.appspot.com",
+  messagingSenderId: "979210237635",
+  appId: "1:979210237635:web:c94f98c7c4ecca1f1b1476",
+  measurementId: "G-CLWLZ7RGM2"
+});
+ 
+export { firebaseConfig as firebase };
