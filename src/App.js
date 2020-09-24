48695cd0e1f57e5be9793f4b2ae3f51218e27e92
casdidier <cheung.didier@gmail.com>
2020-09-24 21:46:25 +0200

    added context

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import React from 'react';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 
+import { ProjectsProvider, SelectedProjectProvider } from './context';
+
 const App = () => {
+
+
   return (
-    <div className="App">
-      <Header></Header>
-      <Content></Content>
-    </div>
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Header></Header>
+          <Content></Content>
+        </div>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
   );
 }
 
