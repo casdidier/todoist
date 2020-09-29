43ccc3d2b30c99680b175c2b04e1f85c573fefcf
casdidier <cheung.didier@gmail.com>
2020-09-29 16:36:30 +0200

    darkmode

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 
 import { ProjectsProvider, SelectedProjectProvider } from './context';
 
-const App = () => {
+const App = ({ darkModeDefault = false }) => {
 
-
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
   return (
     <SelectedProjectProvider>
       <ProjectsProvider>
         <div className="App">
-          <Header></Header>
-          <Content></Content>
+          <main
+            data-testid="application"
+            className={darkMode ? 'darkmode' : undefined}>
+            <Header darkMode={darkMode} setDarkMode={setDarkMode}></Header>
+            <Content></Content>
+          </main>
         </div>
       </ProjectsProvider>
     </SelectedProjectProvider>
