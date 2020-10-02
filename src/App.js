61c1806114ec7d7fc2548551c8e11c691bb2ef8a
casdidier <cheung.didier@gmail.com>
2020-10-02 23:32:40 +0200

    fixed issue with test run

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
-
+import PropTypes from 'prop-types';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
-
 import { ProjectsProvider, SelectedProjectProvider } from './context';
 
-const App = ({ darkModeDefault = false }) => {
-
+export const App = ({ darkModeDefault = false }) => {
   const [darkMode, setDarkMode] = useState(darkModeDefault);
+
   return (
     <SelectedProjectProvider>
       <ProjectsProvider>
-        <div className="App">
-          <main
-            data-testid="application"
-            className={darkMode ? 'darkmode' : undefined}>
-            <Header darkMode={darkMode} setDarkMode={setDarkMode}></Header>
-            <Content></Content>
-          </main>
-        </div>
+        <main
+          data-testid="application"
+          className={darkMode ? 'darkmode' : undefined}
+        >
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Content />
+        </main>
       </ProjectsProvider>
     </SelectedProjectProvider>
   );
-}
+};
 
-export default App;
+App.propTypes = {
+  darkModeDefault: PropTypes.bool,
+};
--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -5,19 +5,19 @@ import { App } from '../App';
 beforeEach(cleanup); // clean meeeeeeeeee!
 
 describe('<App />', () => {
-  // it('renders the application', () => {
-  //   const { queryByTestId } = render(<App />);
-  //   expect(queryByTestId('application')).toBeTruthy();
-  //   expect(
-  //     queryByTestId('application').classList.contains('darkmode')
-  //   ).toBeFalsy();
-  // });
+  it('renders the application', () => {
+    const { queryByTestId } = render(<App />);
+    expect(queryByTestId('application')).toBeTruthy();
+    expect(
+      queryByTestId('application').classList.contains('darkmode')
+    ).toBeFalsy();
+  });
 
-  // it('renders the application using dark mode', () => {
-  //   const { queryByTestId } = render(<App darkModeDefault />);
-  //   expect(queryByTestId('application')).toBeTruthy();
-  //   expect(
-  //     queryByTestId('application').classList.contains('darkmode')
-  //   ).toBeTruthy();
-  // });
+  it('renders the application using dark mode', () => {
+    const { queryByTestId } = render(<App darkModeDefault />);
+    expect(queryByTestId('application')).toBeTruthy();
+    expect(
+      queryByTestId('application').classList.contains('darkmode')
+    ).toBeTruthy();
+  });
 });
